9eac420681918e2e348d1db80d3a268311606060
Nicolás Reynolds <fauno@endefensadelsl.org>
2016-02-02 12:38:45 -0300

    config options

--- lib/jekyll-pandoc-multiple-formats/config.rb
+++ lib/jekyll-pandoc-multiple-formats/config.rb
@@ -2,11 +2,12 @@ module JekyllPandocMultipleFormats
   class Config
     DEFAULTS = {
       'skip'       => false,
-      'permalink'  => ':output_ext/:slug.:output_ext',
+      'bundle_permalink'  => ':output_ext/:slug.:output_ext',
       'papersize'  => 'a5paper',
       'sheetsize'  => 'a4paper',
       'imposition' => true,
       'binder'     => true,
+      'signature'  => 20,
       'flags'      => '--smart',
       'site_flags' => '--toc',
       'outputs'    => { }
@@ -16,6 +17,23 @@ module JekyllPandocMultipleFormats
 
     def initialize(config = {})
       @config = Jekyll::Utils.deep_merge_hashes(DEFAULTS, config)
+    end
+
+    def skip?
+      @config['skip']
+    end
+
+    def imposition?
+      @config['imposition']
+    end
+
+    def binder?
+      @config['binder']
+    end
+
+    # TODO magic
+    def outputs
+      @config['outputs']
     end
   end
 end
